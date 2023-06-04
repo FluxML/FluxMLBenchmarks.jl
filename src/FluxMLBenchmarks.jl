ff617786e4828389aada9bcac80b125ba92fd78a
Kyle Daruwalla <daruwalla@wisc.edu>
2023-06-04 14:32:38 -0400

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.34

---
+++ src/FluxMLBenchmarks.jl
@@ -0,0 +1,5 @@
+module FluxMLBenchmarks
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using FluxMLBenchmarks
+using Test
+
+@testset "FluxMLBenchmarks.jl" begin
+    # Write your tests here.
+end
