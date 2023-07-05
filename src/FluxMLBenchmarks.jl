a8067b11ebb51de159c33ec788447b9931c3a442
skyleaworlder <870033938@qq.com>
2023-07-05 12:30:15 +0000

    test: for parse_enabled_benchmarks and global const variables

--- src/FluxMLBenchmarks.jl
+++ src/FluxMLBenchmarks.jl
@@ -4,8 +4,9 @@ module FluxMLBenchmarks
 include("env_utils.jl")
 export Dependency, get_name, init_dependencies,
     parse_commandline, parse_deps_list,
-    parse_enabled_benchmarks, FLUXML_AVAILABLE_BENCHMARKS,
-    setup_fluxml_env, teardown
+    parse_enabled_benchmarks,
+    setup_fluxml_env, teardown,
+    BENCHMARK_PKG_PATH, BENCHMARK_FILES_PATH, FLUXML_AVAILABLE_BENCHMARKS
 
 include("judge_utils.jl")
 export markdown_report, display_markdown_report
