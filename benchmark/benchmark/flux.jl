ab848db73b4e4411008807f96df7e2eb4270a86d
skyleaworlder <870033938@qq.com>
2023-07-18 14:22:35 +0000

    normalize: benchmark flux

--- benchmark/benchmark/flux.jl
+++ benchmark/benchmark/flux.jl
@@ -6,7 +6,10 @@ model = Chain(
     Dense(64, 10))
 
 SUITE["flux"] = BenchmarkGroup()
+
+########## mlp ############
 SUITE["flux"]["mlp"] = BenchmarkGroup()
+get(ENV, "FLUXML_BENCHMARK_FLUX_MLP", "false") == "true" &&
 for et in (Float16, Float32, Float64)
     x = rand(et, 32, 32, 1, 5)
     SUITE["flux"]["mlp"][string(et)] = @benchmarkable model($x)
