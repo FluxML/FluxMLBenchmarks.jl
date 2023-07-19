ae539119c4b5cd959140e55daba57b108a381bae
skyleaworlder <870033938@qq.com>
2023-07-19 16:43:03 +0000

    fix: DEFAULT_PARAMETERS assignment

--- benchmark/runbenchmarks-pr.jl
+++ benchmark/runbenchmarks-pr.jl
@@ -15,9 +15,6 @@ baseline_url = parsed_args["baseline"]
 setup_fluxml_env([baseline_url])
 
 using BenchmarkTools
-BenchmarkTools.DEFAULT_PARAMETERS.samples = 20
-BenchmarkTools.DEFAULT_PARAMETERS.seconds = 2.5
-
 using PkgBenchmark
 group_baseline = benchmarkpkg(
     dirname(@__DIR__),
@@ -41,9 +38,6 @@ target_url = parsed_args["target"]
 setup_fluxml_env([target_url])
 
 using BenchmarkTools
-BenchmarkTools.DEFAULT_PARAMETERS.samples = 20
-BenchmarkTools.DEFAULT_PARAMETERS.seconds = 2.5
-
 using PkgBenchmark
 group_target = benchmarkpkg(
     dirname(@__DIR__),
