6d28bb7a25f699cc5635b210fceb361492198509
skyleaworlder <870033938@qq.com>
2023-07-27 16:48:00 +0000

    refact: change runbenchmarks-cli.jl function, support multiple sets of dependencies, but not judge and generate report.md any longer

--- benchmark/runbenchmarks-cli.jl
+++ benchmark/runbenchmarks-cli.jl
@@ -1,63 +1,31 @@
 using Pkg
-
-###########################################################################
-
 Pkg.develop(PackageSpec(path = ENV["PWD"]))
 using FluxMLBenchmarks
 
 parsed_args = parse_commandline()
 retune_arg = parsed_args["retune"]
 retune_arg || get_tuning_json()
-enable_arg = parsed_args["enable"]
-disable_arg = parsed_args["disable"]
+enable_arg, disable_arg = parsed_args["enable"], parsed_args["disable"]
 enabled_benchmarks = parse_enabled_benchmarks(enable_arg, disable_arg)
 
 deps_list = parsed_args["deps-list"]
-baseline_fluxml_deps, target_fluxml_deps = parse_deps_list(deps_list)
-time_setup_fluxml_env = @elapsed setup_fluxml_env(baseline_fluxml_deps)
-@info "TIME: setup FluxML benchmarking environment (baseline) cost $time_setup_fluxml_env"
+parsed_deps_list = parse_deps_list(deps_list)
+for (i, deps) in enumerate(parsed_deps_list)
+    time_setup_fluxml_env = @elapsed setup_fluxml_env(deps)
+    @info "($i) TIME: setup FluxML benchmarking environment cost $time_setup_fluxml_env"
+    
+    using PkgBenchmark: benchmarkpkg, BenchmarkConfig
+    time_run_benchmarks = @elapsed begin benchmarkpkg(
+        dirname(@__DIR__),
+        BenchmarkConfig(
+            env = merge(
+                Dict("JULIA_NUM_THREADS" => get(ENV, "JULIA_NUM_THREADS", "1")),
+                enabled_benchmarks
+            )
+        ),
+        resultfile = joinpath(@__DIR__, "result-$i.json")
+    ) end
+    @info "($i) TIME: run benchmarks cost $time_run_benchmarks"
 
-using BenchmarkTools
-using PkgBenchmark
-group_baseline = benchmarkpkg(
-    dirname(@__DIR__),
-    BenchmarkConfig(
-        env = merge(
-            Dict("JULIA_NUM_THREADS" => get(ENV, "JULIA_NUM_THREADS", "1")),
-            enabled_benchmarks
-        )
-    ),
-    resultfile = joinpath(@__DIR__, "result-baseline.json")
-)
-
-teardown()
-
-###########################################################################
-
-Pkg.develop(PackageSpec(path = ENV["PWD"]))
-using FluxMLBenchmarks
-
-time_setup_fluxml_env = @elapsed setup_fluxml_env(target_fluxml_deps)
-@info "TIME: setup FluxML benchmarking environment (target) cost $time_setup_fluxml_env"
-
-using BenchmarkTools
-using PkgBenchmark
-group_target = benchmarkpkg(
-    dirname(@__DIR__),
-    BenchmarkConfig(
-        env = merge(
-            Dict("JULIA_NUM_THREADS" => get(ENV, "JULIA_NUM_THREADS", "1")),
-            enabled_benchmarks
-        )
-    ),
-    resultfile = joinpath(@__DIR__, "result-target.json"),
-)
-
-teardown()
-
-###########################################################################
-
-judgement = judge(group_target, group_baseline)
-report_md = markdown_report(judgement)
-write(joinpath(@__DIR__, "report.md"), report_md)
-display_markdown_report(report_md)
+    teardown()
+end
