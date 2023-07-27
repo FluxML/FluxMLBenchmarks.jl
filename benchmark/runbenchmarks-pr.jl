590c0f8c4b9b287ba915ba197651cd7d5c00c67d
skyleaworlder <870033938@qq.com>
2023-07-27 15:33:24 +0000

    feat: init results uploading

--- benchmark/runbenchmarks-pr.jl
+++ benchmark/runbenchmarks-pr.jl
@@ -13,24 +13,33 @@ disable_arg = parsed_args["disable"]
 enabled_benchmarks = parse_enabled_benchmarks(enable_arg, disable_arg)
 
 baseline_url = parsed_args["baseline"]
-time_setup_fluxml_env = @elapsed setup_fluxml_env([baseline_url])
-@info "TIME: setup FluxML benchmarking environment (baseline) cost $time_setup_fluxml_env"
+try
+    !suitable_to_use_result_cache(baseline_url) && throw(
+        "not suitable_to_use_result_cache, run benchmarks")
 
-using BenchmarkTools
-using PkgBenchmark
-time_run_benchmarks = @elapsed begin group_baseline = benchmarkpkg(
-    dirname(@__DIR__),
-    BenchmarkConfig(
-        env = merge(
-            Dict("JULIA_NUM_THREADS" => get(ENV, "JULIA_NUM_THREADS", "1")),
-            enabled_benchmarks
-        )
-    ),
-    resultfile = joinpath(@__DIR__, "result-baseline.json")
-) end
-@info "TIME: run benchmarks (baseline) cost $time_run_benchmarks"
+    group_baseline = get_benchmarkresults_from_branch(baseline_url)
+    isnothing(group_baseline) && throw(
+        "cannot get result file, run benchmarks")
+catch
+    time_setup_fluxml_env = @elapsed setup_fluxml_env([baseline_url])
+    @info "TIME: setup FluxML benchmarking environment (baseline) cost $time_setup_fluxml_env"
 
-teardown()
+    using BenchmarkTools
+    using PkgBenchmark
+    time_run_benchmarks = @elapsed begin group_baseline = benchmarkpkg(
+        dirname(@__DIR__),
+        BenchmarkConfig(
+            env = merge(
+                Dict("JULIA_NUM_THREADS" => get(ENV, "JULIA_NUM_THREADS", "1")),
+                enabled_benchmarks
+            )
+        ),
+        resultfile = joinpath(@__DIR__, "result-baseline.json")
+    ) end
+    @info "TIME: run benchmarks (baseline) cost $time_run_benchmarks"
+
+    teardown()
+end
 
 ###########################################################################
 
@@ -56,6 +65,10 @@ time_run_benchmarks = @elapsed begin group_target = benchmarkpkg(
 @info "TIME: run benchmarks (target) cost $time_run_benchmarks"
 
 teardown()
+
+if suitable_to_use_result_cache(target_url)
+    push_result(target_url, joinpath(@__DIR__, "result-target.json"))
+end
 
 ###########################################################################
 
